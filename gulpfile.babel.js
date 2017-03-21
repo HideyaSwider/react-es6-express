e91419be53f2be29aa97c98f104d4743f912f338
HideyaBenAbderrahmen <benabderrahmenhideya@gmail.com>
2017-03-21 16:19:50 -0400

    ES6 Gulp Config

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,17 +1,18 @@
 'use strict';
 
-var browserify = require('browserify'),
-	browserSync = require('browser-sync').create(),
-	concat = require('gulp-concat'),
-	eslint = require('gulp-eslint'),
-	gulp = require('gulp'),
-	nodemon = require('gulp-nodemon'),
-	babelify = require('babelify'),
-	runSequence = require('run-sequence'),
-	source = require('vinyl-source-stream'),
-	less = require('gulp-less'),
-	path = require('path'),
-	config = {
+import browserify from 'browserify';
+import BrowserSync from 'browser-sync';
+import concat from 'gulp-concat';
+import eslint from 'gulp-eslint';
+import gulp from 'gulp';
+import nodemon from 'gulp-nodemon';
+import babelify from 'babelify';
+import runSequence from 'run-sequence';
+import source from 'vinyl-source-stream';
+import less from 'gulp-less';
+import path from 'path';
+
+var	config = {
 		port: process.env.PORT || 1991,
 		devBaseUrl: 'http://localhost',
 		paths: {
@@ -21,21 +22,18 @@ var browserify = require('browserify'),
 			dist: './dist',
 			homeJSX: './src/home.jsx'
 		}
-	};
+	},
+	browserSync = BrowserSync.create();
 
-/*
- * Asset Manipulation
- */
-
-gulp.task('html', function() {
+gulp.task('html', () => {
 	console.log('HTML files have been changed... Reloading...');
 	browserSync.reload();
 });
 
-gulp.task('jsx', function() {
+gulp.task('jsx', () => {
 	console.log('JSX files have been changed... Reloading...');
 	browserify(config.paths.homeJSX)
-		.transform("babelify", {presets: ["es2015", "react"]})
+		.transform('babelify', {presets: ['es2015', 'react']})
 		.bundle()
 		.on('error', console.error.bind(console))
 		.pipe(source('home.js'))
@@ -44,24 +42,20 @@ gulp.task('jsx', function() {
 		//todo: lint here too. No, I don't want it to lint in prod.
 });
 
-gulp.task('lint', function() {
+gulp.task('lint', () => {
 	return gulp.src(config.paths.react)
 		.pipe(eslint({configFile: 'eslint.config.json'}))
 		.pipe(eslint.format());
 });
 
-gulp.task('less', function () {
+gulp.task('less', () => {
 	return gulp.src(config.paths.less)
 		.pipe(less({ paths: [path.join(__dirname, 'less', 'includes')] }))
 		.pipe(gulp.dest('./public/stylesheets/'))
 		.pipe(browserSync.stream({match: '**/*.less'}));
 });
 
-
-/*
- * Use it
- */
-gulp.task('node', function() {
+gulp.task('node', () => {
 	nodemon({
 		script: 'bin/www',
 		//verbose: true,
@@ -72,15 +66,15 @@ gulp.task('node', function() {
 			'bin',
 			'routes'
 		]
-	}).on('restart', function() {
+	}).on('restart', () => {
 		console.log('Express files have been changed... Reloading...');
-		setTimeout(function() {
+		setTimeout( () => {
 			browserSync.reload();
 		}, 500);
 	});
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', () => {
 	browserSync.init({
 		browser: 'google chrome',
 		port: 1992,
@@ -92,7 +86,7 @@ gulp.task('watch', function() {
 	gulp.watch(config.paths.react, ['jsx']);
 });
 
-gulp.task('doDev', function() {
+gulp.task('doDev', () => {
 	runSequence(['node', 'watch']);
 });
 
